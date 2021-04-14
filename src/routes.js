a6e48492e24325750cb0b9ba40fecb8691586db6
Leonardo Luz <39962306+leouluz@users.noreply.github.com>
2021-04-14 17:54:06 -0300

    criado estrutura de pastas / e criando estrutura de rotas

---
+++ src/routes.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
+
+import Home from './pages/Home'
+import List from './pages/List'
+import Search from './pages/Search'
+import MapScreen from './pages/Map'
+
+function Tabs() {
+  return (
+    <Tab.Navigator
+      headerMode={false}
+      tabBarOptions={{
+        labelStyle: { fontSize: 20 },
+        style: { justifyContent: 'center' }
+      }}
+    >
+      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen name="List" component={List} />
+      <Tab.Screen name="Map" component={MapScreen} />
+    </Tab.Navigator>
+  )
+}
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator headerMode={false}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Menu" component={Tabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
